6b6c73de8e93086e0e28c9a193d62605ff7f67a8
J&G <denbaguspranata@gmail.com>
2022-06-24 16:02:14 +0700

    Update tokens.js

--- tokens.js
+++ tokens.js
@@ -8,66 +8,21 @@ module.exports = {
         primaryPool: "0x856A1C95bef293de7367b908DF2b63ba30FBdD59"
     },
     feg: {
-        title: "FEG",
+        title: "FEGtoken",
         contract: "0xacFC95585D80Ab62f67A14C566C1b7a49Fe91167",
         decimals: 9,
         burn_wallet: "0x000000000000000000000000000000000000dead",
         canBurn: true,
         primaryPool: "0x2Aa763656A92ce1e6E560c3EA76b4C5fca7C6C14"
     },
-    enh: {
-        title: "Enhance",
-        contract: "0xCb6a0c342e0968b3D4AC0C90b8da4edBA74f0385",
-        decimals: 18,
-        burn_wallet: "0x000000000000000000000000000000000000dead",
-        canBurn: true,
-        primaryPool: "0x7a06e45080C2DFC142077a747a35f99c5647493A"
-    },
-    affinity: {
-        title: "Affinity",
-        contract: "0x0cAE6c43fe2f43757a767Df90cf5054280110F3e",
+    
+    
+    cake: {
+        title: "Cake",
+        contract: "0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82",
         decimals: 9,
         burn_wallet: "0x000000000000000000000000000000000000dead",
         canBurn: true,
-        primaryPool: "0x2a7241d85857484dBB17ea5f96C591378A85f652"
-    },
-    egc: {
-        title: "EverGrow",
-        contract: "0xC001BBe2B87079294C63EcE98BdD0a88D761434e",
-        decimals: 9,
-        burn_wallet: "0x000000000000000000000000000000000000dead",
-        canBurn: true,
-        primaryPool: "0xD254a3C351DAd83F8B369554B420047A1ED60f1C"
-    },
-    puli: {
-        title: "Puli-Inu",
-        contract: "0xAEf0a177c8c329CBC8508292Bb7E06c00786BBfC",
-        decimals: 9,
-        canBurn: false,
-        primaryPool: "0xAEf0a177c8c329CBC8508292Bb7E06c00786BBfC"
-    },
-    cryft: {
-        title: "Cryft",
-        contract: "0xD2267477A35DC048B41AF9c96b0405E0717CA1Bf",
-        decimals: 9,
-        burn_wallet: "0x0000000000000000000000000000000000000001",
-        canBurn: true,
-        primaryPool: "0xD2267477A35DC048B41AF9c96b0405E0717CA1Bf"
-    },
-    luna: {
-        title: "Luna",
-        contract: "0x156ab3346823B651294766e23e6Cf87254d68962",
-        decimals: 9,
-        burn_wallet: "0x0000000000000000000000000000000000000001",
-        canBurn: true,
-        primaryPool: "0xEB8d08030017BD1362a5414112CaCb094FA20cE1"
-    },
-    ezi: {
-        title: "EZillion",
-        contract: "0xCa0C1c12466A57b26850b05a0BA2Fb39F9763A0c",
-        decimals: 9,
-        burn_wallet: "0x000000000000000000000000000000000000dead",
-        canBurn: true,
-        primaryPool: "0x9cC45a53C0e7Ef1f07640569639ac4Cfe7117F75"
+        primaryPool: "0x0eD7e52944161450477ee417DE9Cd3a859b14fD0"
     }
 }
