f34519842c8ab73575cda7d9fc18d5c44b33f5cf
J&G <denbaguspranata@gmail.com>
2022-06-26 21:52:07 +0700

    Update search.js

--- components/tracker/search.js
+++ components/tracker/search.js
@@ -6,7 +6,7 @@ const symbols = Object.keys(tokens);
 
 export default function SearchBar({ defaultValue, activeToken }) {
 
-    const [active, setActive] = useState(activeToken ? activeToken : "sfm");
+    const [active, setActive] = useState(activeToken ? activeToken : "feg");
 
     useEffect(async() => {
 
